7cee01550ef3e8cc5131557b3ae01cb4fc2fcbde
Viraagitta <viraagitta99@gmail.com>
2022-08-16 00:49:50 +0700

    fix: revisi navigate reimburse

--- staff-mobile/src/store/action/index.js
+++ staff-mobile/src/store/action/index.js
@@ -8,7 +8,7 @@ import {
   LOGIN_STAFF,
 } from "./actionType";
 import axios from "axios";
-const baseUrl = "http://localhost:3000";
+const baseUrl = "http://192.168.100.13:3000";
 export const loginSuccess = (payload) => {
   return {
     type: LOGIN_STAFF,
